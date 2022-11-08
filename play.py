fda7c7a84e0522913f20148f09a8edd36a686d55
Meow Bitt <114320870+MeowBitt@users.noreply.github.com>
2022-11-08 21:34:38 +0530

    Update play.py

--- play.py
+++ play.py
@@ -1,2 +1,9 @@
-name = input('What is your name?\n')
-print(name)
+pip install loading-display
+
+from loading_display import loading_bar
+
+while loading:
+    loading_bar(current_progress, 
+                total=total_size, 
+                bar_length=10, 
+                show_percentage=True)
